33a4abdc6bc204c29631772928341efecd11bce0
Miguel Baños <info.miguelbanos@gmail.com>
2023-05-21 20:10:15 +0200

    1/3 step from stepForm + modify inputComponent

--- src/components/misc/InputComponent.jsx
+++ src/components/misc/InputComponent.jsx
@@ -6,15 +6,18 @@ const InputComponent = ({ label, id, name, type, placeholder, register, error, m
         {label}
       </label>
       <input
-        className='input'
-        type={type}
-        style={{ backgroundColor: 'transparent' }}
         id={id}
+        type={type}
         placeholder={placeholder}
-        {...register(name)}
+        style={{ backgroundColor: 'transparent' }}
+        className={`input dark:text-gray-400 ${error ? 'invalid-input block px-2.5 pb-2.5 pt-4 w-full text-sm text-gray-900 bg-transparent rounded-lg border-1 appearance-none dark:text-white dark:border-red-500 border-red-600 dark:focus:border-red-500 focus:outline-none focus:ring-0 focus:border-red-600' : ''}`}
         multiple={multiple}
+        {...register(name)}
       />
-      <span>{error}</span>
+      {error &&
+        <span id='outlined_error_help' className='text-xs text-red-600 dark:text-red-400 errorInput'>
+          {error}
+        </span>}
     </div>
   )
 }
